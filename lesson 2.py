04cdcf5310d827b580c754880092124223167693
Lena-vetochka <elenanechaeva1701@yandex.ru>
2024-08-15 06:37:09 +0300

    Add files via upload

---
+++ lesson 2.py
@@ -0,0 +1,7 @@
+
+homework = 12
+time_hour = 1.5
+course = 'Python'
+time_1_homework = (time_hour / homework)
+print('Курс:',course,', всего задач:',homework,', затрачено часов:',time_hour,
+      ', среднее время выполнения',time_1_homework, 'часа.')
